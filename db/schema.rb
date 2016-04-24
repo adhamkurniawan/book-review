34d61e492b48d2e2411f735b2f13e2100ae02aa8
Adam Kurniawan <dmkrnwn@gmail.com>
2016-04-24 14:48:53 +0700

    Added migration name to user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160423043652) do
+ActiveRecord::Schema.define(version: 20160424073921) do
 
   create_table "books", force: :cascade do |t|
     t.string   "title"
@@ -55,6 +55,7 @@ ActiveRecord::Schema.define(version: 20160423043652) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
